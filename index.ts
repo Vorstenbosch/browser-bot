49725f5056b6f0649b23c2d4158ee00a9ec91cb5
Stijn Vorstenbosch <vorstenbosch.stijn@gmail.com>
2022-10-20 21:42:47 +0200

    chore: clean up

---
+++ index.ts
@@ -0,0 +1,34 @@
+
+import executor from "./lib/executor"
+import { Flow } from "./lib/types/flow"
+import getDriver from "./lib/driver/driver" 
+
+getDriver({}).then(driver => {
+    const flow: Flow = {
+        name: "test-run",
+        actions: [
+            {
+                type: "NAVIGATE",
+                parameters: {
+                    url: "https://aap.nl"
+                }
+            },
+            {
+                type: "GET_DATA",
+                parameters: {
+                    xpath: '//*[@id="main"]/div/div/section/div/div/div/div/div/div/div/div/div/div/section[3]/div/div/div/div/div/div/div/h2',
+                    dataKey: "message"
+                },
+            },
+            {
+                type: "CLOSE",
+            },
+
+        ]
+    }
+    
+    executor(driver, flow).then(result => {
+        console.info(JSON.stringify(result, null, 2))
+    })
+})
+
